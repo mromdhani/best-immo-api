1a134e4d4a6d945497e9a0b45fd1d06270b39734
Mohamed Romdhani <mromdhani7@gmail.com>
2020-11-23 10:00:43 +0100

    Mon Commit initial

---
+++ src/main/java/be/businesstraining/bestimmoapi/BestImmoApiApplication.java
@@ -0,0 +1,13 @@
+package be.businesstraining.bestimmoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestImmoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestImmoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/businesstraining/bestimmoapi/BestImmoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.businesstraining.bestimmoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestImmoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
